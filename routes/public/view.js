b566f5043893a3ed427b383a68da66f247bcbbc6
fawzy75 <azazazazazsx406@gmail.com>
2024-12-19 17:53:56 +0200

    edits ba3d marawan

--- routes/public/view.js
+++ routes/public/view.js
@@ -1,20 +1,24 @@
 function handlePublicFrontEndView(app) {
   
-    app.get('/', function(req, res) {
-        return res.render('homePage');
-      });
+  app.get('/', function(req, res) {
+      return res.render('welcom_page');
+    });
+    app.get('/homePage', function(req, res) {
+      return res.render('homePage');
+    });
     
-      app.get('/register', function(req, res) {
-        return res.render('register');
-      });
-      
-      app.get('/login', function(req, res) {
-        return res.render('login');
-      });
-      app.get('/image_uplaod', function(req, res) {
-        return res.render('image_uplaod');
-      });
-   
-    }  
   
+    app.get('/register', function(req, res) {
+      return res.render('register');
+    });
+    
+    app.get('/login', function(req, res) {
+      return res.render('login');
+    });
+    app.get('/image_uplaod', function(req, res) {
+      return res.render('image_uplaod');
+    });
+ 
+  }  
+
 module.exports = {handlePublicFrontEndView};
