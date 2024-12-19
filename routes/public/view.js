383da28bbc013a76ec31941504e6b4861260e92e
mohamed <mohamed.w.zoher@gmail.com>
2024-12-19 19:47:55 +0200

    In homepage I make a details button when user click on it transfers to another page that contain all details of equipment , i make a new page called equipmentDetails.hjs in view.js and make changs in public/src/homePage.js and in routes/public/view.js

--- routes/public/view.js
+++ routes/public/view.js
@@ -1,3 +1,4 @@
+const db = require('../../connectors/db');
 function handlePublicFrontEndView(app) {
   
     app.get('/', function(req, res) {
@@ -14,6 +15,21 @@ function handlePublicFrontEndView(app) {
       app.get('/image_uplaod', function(req, res) {
         return res.render('image_uplaod');
       });
+      app.get('/equipments/:id', async (req, res) => {
+        try {
+            const equipmentId = req.params.id;
+            const equipment = await db('equipments').where('equipment_id', equipmentId).first();
+    
+            if (!equipment) {
+                return res.status(404).send('Equipment not found');
+            }
+    
+            res.render('equipmentDetails', { equipment });
+        } catch (error) {
+            console.error('Server error:', error.message);
+            res.status(500).send('Server error');
+        }
+    });
    
     }  
   
